1ddfc6a8f5da87ce53b8e3f638cfd46b85742ecb
briandyko <briandyko@gmail.com>
2017-10-05 15:14:30 -0400

    intro to states, properties, behaviors, constructors

--- IntrotoOOP/Cat.cs
+++ IntrotoOOP/Cat.cs
@@ -8,5 +8,29 @@ namespace IntrotoOOP
 {
     class Cat
     {
+        //STATES
+        //fields
+        private string name;
+
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+
+        //BEHAVIORS
+
+
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor because it takes no parameters in the parentheses
+        }
+
     }
 }
